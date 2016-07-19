4d06e29dbb45d8fe6e684f62eca51563c45e5c8e
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2016-07-19 16:31:49 -0400

    Show correct legend. Closes #1

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -17,6 +17,32 @@ chrome.extension.sendMessage({}, function(response) {
           image.src = '/images/status3.gif';
         }
       });
+
+      var tds = document.querySelectorAll('body > div > div .pad8.bordered:not(.gradient):not(.whitebg) td');
+      var iteration = 0;
+      var messages = {
+        0: 'Service is operating normally',
+        1: 'Everything is fucked',
+        2: 'We finally concede<br />everything is fucked',
+        3: 'Nuclear armageddon<br />wiped out all life on Earth',
+      };
+      [].forEach.call(tds, function(el) {
+        var image = el.getElementsByTagName('img');
+        if (image.length > 0) {
+          if (iteration == 1) {
+            image[0].src ='/images/status1.gif';
+          }
+          if (iteration == 2) {
+            image[0].src ='/images/status2.gif';
+          }
+          if (iteration == 3) {
+            image[0].src ='/images/status3.gif';
+          }
+          return;
+        }
+        el.innerHTML = messages[iteration];
+        iteration++;
+      });
     }
   }, 10);
 });
